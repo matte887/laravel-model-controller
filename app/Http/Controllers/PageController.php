9e090ae5166945f9c36c2214df0dad50b33f6591
matte887 <m.erroi92@gmail.com>
2022-06-28 14:57:37 +0200

    Instaurata connessione con db e passati dati al frontend

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller {
     public function index() {
-        return view('movies');
+        $movies = Movie::all();
+        // dd($movies);
+        $data = [
+            'movies' => $movies
+        ];
+        return view('movies', $data);
     }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,1 +1,7 @@
 <h1>Lista film</h1>
+{{-- {{dd($movies)}} --}}
+<ul>
+    @foreach ($movies as $this_movie)
+        <li>{{$this_movie->title}}</li>
+    @endforeach
+</ul>
