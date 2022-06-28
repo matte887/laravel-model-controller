89c21094f853c1989c87f215114bd403ff390015
matte887 <m.erroi92@gmail.com>
2022-06-28 15:14:24 +0200

    Mostrati dettagli film

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,6 +2,12 @@
 {{-- {{dd($movies)}} --}}
 <ul>
     @foreach ($movies as $this_movie)
-        <li>{{$this_movie->title}}</li>
+        <li>
+            <h3>{{ $this_movie->title }}</h3>
+            <p>Original title: {{ $this_movie->original_title }}</p>
+            <p>Paese d'origine: {{ $this_movie->nationality }}</p>
+            <p>Data rilascio: {{ $this_movie->date }}</p>
+            <p>Vote: {{ $this_movie->vote }}</p>
+        </li>
     @endforeach
 </ul>
